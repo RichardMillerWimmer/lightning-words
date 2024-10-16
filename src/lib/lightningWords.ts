fb349d2b55b38b8b01ebadd762e5069c9d223b4f
Richard Miller <richardmmill@gmail.com>
2024-10-16 13:15:24 -0400

    init lightning words object

---
+++ src/lib/lightningWords.ts
@@ -0,0 +1,16 @@
+type LightningWordsType = {
+    [key: number]: string[];
+};
+
+export const lightningWords: LightningWordsType = {
+    1: ['the', 'will', 'and', 'is', 'in', 'it', ],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [], 
+    7: [],
+    8: [],
+    9: [],
+    10: [],
+}
