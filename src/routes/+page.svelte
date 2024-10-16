5ea368d15be514006269292547af734eded298b3
Richard Miller <richardmmill@gmail.com>
2024-10-16 13:16:38 -0400

    render words on page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+	import { generateWordTable } from "$lib/generateWordTable";
+
+</script>
+<h1>Lightning Words</h1>
+<span>
+    {generateWordTable(1)}
+</span>
