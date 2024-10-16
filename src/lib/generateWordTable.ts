460aac18853753a3d7effb35640fe311756c5b54
Richard Miller <richardmmill@gmail.com>
2024-10-16 13:16:21 -0400

    generateWordTable

---
+++ src/lib/generateWordTable.ts
@@ -0,0 +1,11 @@
+import { lightningWords } from "./lightningWords";
+
+export const generateWordTable = (week: number): string[] => { 
+    const selectedWeek = lightningWords[week];
+    const randomWordTable = []; 
+    for (let i = 0; i < 20; i++) {
+        const randomIndex = Math.floor(Math.random() * selectedWeek.length);
+        randomWordTable.push(selectedWeek[randomIndex]);
+    }
+    return randomWordTable;
+}
