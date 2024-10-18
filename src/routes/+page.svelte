003e1243af88bed094e89354e6cd9915f8f02f50
Richard Miller <richardmmill@gmail.com>
2024-10-18 18:20:13 -0400

    use subscriber

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,17 @@
 
 	import { generateWordTable } from "$lib/generateWordTable";
 	import { weeks } from "$lib/weeks";
+    import { weekState } from "$lib/createWeek";
+
+    weekState.subscribe((/** @type {number} */ value) => {
+        console.log(value);
+    });
 
 </script>
 <h1>Lightning Words</h1>
 <form>
     <label for="Week">Week:</label>
-    <select name="Week" id="Week">
+    <select name="Week" id="Week" on:change={(event) => weekState.setWeek(+event?.currentTarget?.value)}>
         {#each weeks as week}
             <option value={week}>{week}</option>
         {/each}
