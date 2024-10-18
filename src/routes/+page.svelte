2bfaef2c70da7d233d79fea146a957c66776e2aa
Richard Miller <richardmmill@gmail.com>
2024-10-18 08:39:56 -0400

    add weeks and dropdown select

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,18 @@
 <script>
 
 	import { generateWordTable } from "$lib/generateWordTable";
+	import { weeks } from "$lib/weeks";
 
 </script>
 <h1>Lightning Words</h1>
+<form>
+    <label for="Week">Week:</label>
+    <select name="Week" id="Week">
+        {#each weeks as week}
+            <option value={week}>{week}</option>
+        {/each}
+    </select>
+</form>
 <span>
     {generateWordTable(1)}
 </span>
