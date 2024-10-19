b69694d803c8f7f594b1b82f9c4c0b259820d81c
Richard Miller <richardmmill@gmail.com>
2024-10-19 07:58:17 -0400

    breakout select

---
+++ src/lib/components/Select/Select.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { weekState } from "$lib/createWeek";
+	import { weeks } from "$lib/weeks";
+
+
+</script>
+
+<form>
+    <label for="Week">Week:</label>
+    <select name="Week" id="Week" on:change={(event) => weekState.setWeek(+event?.currentTarget?.value)}>
+        {#each weeks as week}
+            <option value={week}>{week}</option>
+        {/each}
+    </select>
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
 
 	import { generateWordTable } from "$lib/generateWordTable";
-	import { weeks } from "$lib/weeks";
     import { weekState } from "$lib/createWeek";
+    import Select from "$lib/components/Select/Select.svelte";
 
     weekState.subscribe((/** @type {number} */ value) => {
         console.log(value);
@@ -10,14 +10,7 @@
 
 </script>
 <h1>Lightning Words</h1>
-<form>
-    <label for="Week">Week:</label>
-    <select name="Week" id="Week" on:change={(event) => weekState.setWeek(+event?.currentTarget?.value)}>
-        {#each weeks as week}
-            <option value={week}>{week}</option>
-        {/each}
-    </select>
-</form>
+<Select />
 <span>
     {generateWordTable(1)}
 </span>
