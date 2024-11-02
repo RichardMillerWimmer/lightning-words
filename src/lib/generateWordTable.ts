21540a84b398cea7a6279e4b4be6310dcec33560
Richard Miller <richardmmill@gmail.com>
2024-11-02 06:57:27 -0400

    set word table count

--- src/lib/generateWordTable.ts
+++ src/lib/generateWordTable.ts
@@ -3,7 +3,7 @@ import { lightningWords } from "./lightningWords";
 export const generateWordTable = (week: number): string[] => { 
     const selectedWeek = lightningWords[week];
     const randomWordTable = []; 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 40; i++) {
         const randomIndex = Math.floor(Math.random() * selectedWeek.length);
         randomWordTable.push(selectedWeek[randomIndex]);
     }
